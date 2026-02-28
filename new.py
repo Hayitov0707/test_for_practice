5a7c96a4c45f3cfb043d396ed5571a8a1a8f967b
Hayitov <orazhayitov47@gmail.com>
2026-02-28 15:31:35 +0000

    Initial commit

---
+++ new.py
@@ -0,0 +1,8 @@
+import random
+
+options = ['Rock', 'Paper', 'Scissors']
+
+# Select a random element from the list
+computer_choice = random.choice(options)
+
+print(f"The computer chose {computer_choice}")
