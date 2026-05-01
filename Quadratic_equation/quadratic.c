5daf4825329590ed14fdd98bde48da141c3d6a07
Hayitov <orazhayitov47@gmail.com>
2026-05-01 10:18:43 +0000

    Adding files

---
+++ Quadratic_equation/quadratic.c
@@ -0,0 +1,34 @@
+#include "quadratic.h"
+#include <math.h>
+
+int quadratic(double a, double b, double c, double roots[2]) {
+    if (fabs(a) < EPS) return -1;
+
+    double D = b * b - 4.0 * a * c;
+
+    if (fabs(D) < EPS) D = 0.0;
+
+    if (D < 0.0) return 0;
+
+    if (D == 0.0) {
+        roots[0] = -b / (2.0 * a);
+        return 1;
+    }
+
+    double sqrtD = sqrt(D);
+
+    double x1 = (-b - sqrtD) / (2.0 * a);
+    double x2 = (-b + sqrtD) / (2.0 * a);
+
+    if (x1 > x2) {
+        double t = x1;
+        x1 = x2;
+        x2 = t;
+    }
+
+    roots[0] = x1;
+    roots[1] = x2;
+
+    return 2;
+}
+
---
+++ Quadratic_equation/quadratic.h
@@ -0,0 +1,8 @@
+#ifndef QUADRATIC_H
+#define QUADRATIC_H
+
+#define EPS 1e-12
+
+int quadratic(double a, double b, double c, double roots[2]);
+
+#endif
