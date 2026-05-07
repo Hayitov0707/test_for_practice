71178bf9a4966291bfd74f3a0dd5c3f7e293b6a6
Hayitov <orazhayitov47@gmail.com>
2026-05-07 09:40:51 +0000

    Simple changes

--- Integral/integral.h
+++ Integral/integral.h
@@ -1,14 +1,13 @@
 #ifndef INTEGRAL_H
 #define INTEGRAL_H
 
-// Чтобы M_PI был доступен во всех файлах
+
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif
 
 double integral(double (*f)(double), double a, double b, int n);
 
-// тестовые функции
 double f1(double x);
 double f2(double x);
 
