833bd74c5c4466ceca665867bfa2ba296d384e99
Hayitov <orazhayitov47@gmail.com>
2026-05-16 16:43:08 +0000

    Fixing Quadratic branch

--- Quadratic_equation/quadratic.c
+++ Quadratic_equation/quadratic.c
@@ -2,33 +2,35 @@
 #include <math.h>
 
 int quadratic(double a, double b, double c, double roots[2]) {
-    if (fabs(a) < EPS) return -1;
+  if (fabs(a) < EPS)
+    return -1;
 
-    double D = b * b - 4.0 * a * c;
+  double D = b * b - 4.0 * a * c;
 
-    if (fabs(D) < EPS) D = 0.0;
+  if (fabs(D) < EPS)
+    D = 0.0;
 
-    if (D < 0.0) return 0;
+  if (D < 0.0)
+    return 0;
 
-    if (D == 0.0) {
-        roots[0] = -b / (2.0 * a);
-        return 1;
-    }
+  if (D == 0.0) {
+    roots[0] = -b / (2.0 * a);
+    return 1;
+  }
 
-    double sqrtD = sqrt(D);
+  double sqrtD = sqrt(D);
 
-    double x1 = (-b - sqrtD) / (2.0 * a);
-    double x2 = (-b + sqrtD) / (2.0 * a);
+  double x1 = (-b - sqrtD) / (2.0 * a);
+  double x2 = (-b + sqrtD) / (2.0 * a);
 
-    if (x1 > x2) {
-        double t = x1;
-        x1 = x2;
-        x2 = t;
-    }
+  if (x1 > x2) {
+    double t = x1;
+    x1 = x2;
+    x2 = t;
+  }
 
-    roots[0] = x1;
-    roots[1] = x2;
+  roots[0] = x1;
+  roots[1] = x2;
 
-    return 2;
+  return 2;
 }
-
--- Quadratic_equation/test_quadratic.c
+++ Quadratic_equation/test_quadratic.c
@@ -1,68 +1,68 @@
-#include <stdio.h>
-#include <math.h>
 #include "quadratic.h"
+#include <math.h>
+#include <stdio.h>
 
 int passed = 0;
 int failed = 0;
 
-void check_int(const char* name, int res, int exp) {
-    if (res == exp) {
-        printf("✔ %s PASSED\n", name);
-        passed++;
-    } else {
-        printf("❌ %s FAILED (%d != %d)\n", name, res, exp);
-        failed++;
-    }
+void check_int(const char *name, int res, int exp) {
+  if (res == exp) {
+    printf("✔ %s PASSED\n", name);
+    passed++;
+  } else {
+    printf("❌ %s FAILED (%d != %d)\n", name, res, exp);
+    failed++;
+  }
 }
 
-void check_double(const char* name, double res, double exp) {
-    if (fabs(res - exp) < EPS) {
-        printf("✔ %s PASSED\n", name);
-        passed++;
-    } else {
-        printf("❌ %s FAILED (%g != %g)\n", name, res, exp);
-        failed++;
-    }
+void check_double(const char *name, double res, double exp) {
+  if (fabs(res - exp) < EPS) {
+    printf("✔ %s PASSED\n", name);
+    passed++;
+  } else {
+    printf("❌ %s FAILED (%g != %g)\n", name, res, exp);
+    failed++;
+  }
 }
 
 void test1() {
-    double r[2];
-    check_int("a=0", quadratic(0,1,1,r), -1);
+  double r[2];
+  check_int("a=0", quadratic(0, 1, 1, r), -1);
 }
 
 void test2() {
-    double r[2];
-    int n = quadratic(1,0,-1,r);
-    check_int("two roots", n, 2);
-    check_double("x1", r[0], -1);
-    check_double("x2", r[1], 1);
+  double r[2];
+  int n = quadratic(1, 0, -1, r);
+  check_int("two roots", n, 2);
+  check_double("x1", r[0], -1);
+  check_double("x2", r[1], 1);
 }
 
 void test3() {
-    double r[2];
-    int n = quadratic(1,0,0,r);
-    check_int("one root", n, 1);
-    check_double("root", r[0], 0);
+  double r[2];
+  int n = quadratic(1, 0, 0, r);
+  check_int("one root", n, 1);
+  check_double("root", r[0], 0);
 }
 
 void test4() {
-    double r[2];
-    check_int("no roots", quadratic(1,0,1,r), 0);
+  double r[2];
+  check_int("no roots", quadratic(1, 0, 1, r), 0);
 }
 
 void test5() {
-    double r[2];
-    int n = quadratic(1,0,-1e-13,r);
-    check_int("eps root", n, 1);
+  double r[2];
+  int n = quadratic(1, 0, -1e-13, r);
+  check_int("eps root", n, 1);
 }
 
 int main() {
-    test1();
-    test2();
-    test3();
-    test4();
-    test5();
+  test1();
+  test2();
+  test3();
+  test4();
+  test5();
 
-    printf("\npassed=%d failed=%d\n", passed, failed);
-    return failed;
+  printf("\npassed=%d failed=%d\n", passed, failed);
+  return failed;
 }
